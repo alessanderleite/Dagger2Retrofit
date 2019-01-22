02608d1c043fdc33571d7d767166ef15ebfb7586
alessanderleite <alessander.logistica@gmail.com>
2019-01-21 22:20:09 -0200

    create the class named ApiComponent and define all the modules using the @Component annotation

---
+++ app/src/main/java/br/com/alessanderleite/dagger2retrofit/ApiComponent.java
@@ -0,0 +1,11 @@
+package br.com.alessanderleite.dagger2retrofit;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {AppModule.class, ApiModule.class})
+public interface ApiComponent {
+    void inject(MainActivity activity);
+}
