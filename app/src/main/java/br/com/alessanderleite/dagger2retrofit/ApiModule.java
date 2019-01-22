59ad621e369fc35d0e5063498a077411c5f8063e
alessanderleite <alessander.logistica@gmail.com>
2019-01-21 22:18:37 -0200

    add the @module annotation on the ApiModule class

--- app/src/main/java/br/com/alessanderleite/dagger2retrofit/ApiModule.java
+++ app/src/main/java/br/com/alessanderleite/dagger2retrofit/ApiModule.java
@@ -8,12 +8,14 @@ import com.google.gson.GsonBuilder;
 
 import javax.inject.Singleton;
 
+import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+@Module
 public class ApiModule {
 
     String mBaseUrl;
